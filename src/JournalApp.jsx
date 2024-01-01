12be0e83856bc07045e5701fe2ca32f914a3c6eb
unknown <admin@C10-CENT-P-080.lapromotora.com.bo>
2024-01-01 19:33:03 -0400

    #change: paths to LoginPage, RegisterPage and JournalPages were created

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { AppRouter } from "./router/AppRouter";
 
 const JournalApp = () => {
-  return <>Hola mundo</>;
+  return (
+    <>
+      <AppRouter />
+    </>
+  );
 };
 
 export default JournalApp;
---
+++ src/journal/pages/JournalPages.jsx
@@ -0,0 +1,3 @@
+export const JournalPages = () => {
+  return <div>JournalPages</div>;
+};
