2dbe5d7c8cc0b55bb896e1bd12a611bbbeead68b
kali user <kali@kali.kali.org>
2024-02-09 23:35:04 -0400

    #change. SideBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,6 @@
-import { Box } from "@mui/material";
+import { Box, Toolbar } from "@mui/material";
 import { NavBar } from "../components/NavBar";
+import { SideBar } from "../components/SideBar";
 
 const drawerWidth = 240;
 
@@ -10,8 +11,11 @@ export const JournalLayout = ({ children }) => {
       <NavBar drawerWidth={drawerWidth} />
 
       {/* Sidebar */}
+      <SideBar />
+
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {/* Toolbar */}
+        <Toolbar />
 
         {children}
       </Box>
