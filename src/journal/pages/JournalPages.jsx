9b7eef0f4c3c770595287c728f168f829e8d7902
kali user <kali@kali.kali.org>
2024-02-09 23:03:48 -0400

    #change. JournalLayout and JournalPage

--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -1,11 +1,15 @@
-import { MailOutline } from "@mui/icons-material";
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPages = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPages</Typography>
-      <MailOutline />
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        incidunt quidem quod dolores consequatur dignissimos omnis cupiditate
+        earum, recusandae optio voluptates corporis nobis accusantium veritatis
+        sint autem sed excepturi et?
+      </Typography>
+    </JournalLayout>
   );
 };
