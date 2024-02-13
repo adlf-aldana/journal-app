0250b090de33a314070aadf9643db66fec967ae8
kali user <kali@kali.kali.org>
2024-02-12 23:37:12 -0400

    #change. user registration form

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,11 +1,25 @@
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from "../layout/AuthLayout";
 import { Link as RouterLink } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+
+const formData = {
+  email: "adlf.aldana@gmail.com",
+  password: "123456",
+  displayName: "Adolfo Aldana",
+};
 
 export const RegisterPage = () => {
+  const { displayName, email, password, onInputChange, formState } =
+    useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
   return (
     <AuthLayout title="Crear Cuenta">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
@@ -13,6 +27,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Nombre completo"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -21,6 +38,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="correo@google.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
@@ -29,12 +49,15 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-            <Grid item xs={12} sm={6}>
-              <Button variant="contained" fullWidth>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" fullWidth>
                 Crear cuenta
               </Button>
             </Grid>
