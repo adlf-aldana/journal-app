e24f7cfefec8c88bb4072f8e65c28418e2ec354f
kali user <kali@kali.kali.org>
2024-02-21 07:31:24 -0400

    #change. Custom Hook for Authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { CheckingAuth } from "../ui";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "../firebase/config";
+import { login, logout } from "../store/auth";
+import { useEffect } from "react";
 
 export const AppRouter = () => {
   const { status } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(FirebaseAuth, async (user) => {
+      if (!user) return dispatch(logout());
+
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+  }, [status]);
 
   if (status === "checking") {
     return <CheckingAuth />;
@@ -13,10 +27,18 @@ export const AppRouter = () => {
 
   return (
     <Routes>
+      {status === "authenticated" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
+
+      <Route path="*" element={<Navigate to="/auth/login" />} />
+
       {/* Login y Registro */}
-      <Route path="/auth/*" element={<AuthRoutes />} />
+      {/* <Route path="/auth/*" element={<AuthRoutes />} /> */}
       {/* JournalApp */}
-      <Route path="/*" element={<JournalRoutes />} />
+      {/* <Route path="/*" element={<JournalRoutes />} /> */}
     </Routes>
   );
 };
