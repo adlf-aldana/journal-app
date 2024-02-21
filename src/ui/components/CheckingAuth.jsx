ac2cd38d948a4f5a119f13674a0ab80fd7e7c311
kali user <kali@kali.kali.org>
2024-02-21 07:16:35 -0400

    #change. Checking authentication

---
+++ src/ui/components/CheckingAuth.jsx
@@ -0,0 +1,26 @@
+import { CircularProgress, Grid } from "@mui/material";
+import React from "react";
+
+export const CheckingAuth = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid
+        item
+        direction="row"
+        justifyContent="center"
+        sx={{
+          width: { md: 450 },
+        }}
+      >
+        <CircularProgress color="warning" />
+      </Grid>
+    </Grid>
+  );
+};
