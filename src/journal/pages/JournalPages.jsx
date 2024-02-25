0dce0cfc9fd8ea3c61b9a61e87fb4ba63c5c89aa
kali user <kali@kali.kali.org>
2024-02-25 12:23:02 -0400

    #change. Create a new note

--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -2,14 +2,22 @@ import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 import { AddOutlined } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import { startNewNote } from "../../store/journal/thunks";
 
 export const JournalPages = () => {
+  const dispatch = useDispatch();
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     <JournalLayout>
       <NothingSelectedView />
       {/* <NoteView /> */}
 
       <IconButton
+        onClick={onClickNewNote}
         size="large"
         sx={{
           color: "white",
