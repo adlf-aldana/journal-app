c4d29d2fd94975edec359d9b7e4be493441b88e9
kali user <kali@kali.kali.org>
2024-02-26 08:05:00 -0400

    #change. Activate the created note

--- src/journal/pages/JournalPages.jsx
+++ src/journal/pages/JournalPages.jsx
@@ -2,21 +2,22 @@ import { IconButton } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 import { AddOutlined } from "@mui/icons-material";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { startNewNote } from "../../store/journal/thunks";
 
 export const JournalPages = () => {
   const dispatch = useDispatch();
+  const { isSaving, active } = useSelector((state) => state.journal);
   const onClickNewNote = () => {
     dispatch(startNewNote());
   };
 
   return (
     <JournalLayout>
-      <NothingSelectedView />
-      {/* <NoteView /> */}
+      {!!active ? <NoteView /> : <NothingSelectedView />}
 
       <IconButton
+        disabled={isSaving}
         onClick={onClickNewNote}
         size="large"
         sx={{
--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,8 +1,10 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
+import { addNewEmptyNote, savingNewNote, setActiveNote } from "./";
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
+    dispatch(savingNewNote());
     //uid
     const { uid } = getState().auth;
 
@@ -14,8 +16,11 @@ export const startNewNote = () => {
 
     const newDoc = doc(collection(FirebaseDB, `${uid}/journal/notes`));
     await setDoc(newDoc, newNote);
+
+    newNote.id = newDoc.id;
+
     //! dispatch
-    // dispatch(newNote);
-    //
+    dispatch(addNewEmptyNote(newNote));
+    dispatch(setActiveNote(newNote));
   };
 };
