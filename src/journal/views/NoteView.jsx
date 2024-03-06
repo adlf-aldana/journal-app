74a2989f9edd750761133f42ffb6ce0b55ecc8cb
kali user <kali@kali.kali.org>
2024-03-06 07:56:41 -0400

    #change. SweerAlert2

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -7,10 +7,16 @@ import { useMemo } from "react";
 import { useEffect } from "react";
 import { setActiveNote } from "../../store/journal/journalSlice";
 import { startSaveNote } from "../../store/journal";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.css";
 
 export const NoteView = () => {
   const dispatch = useDispatch();
-  const { active: note } = useSelector((state) => state.journal);
+  const {
+    active: note,
+    messageSaved,
+    isSaving,
+  } = useSelector((state) => state.journal);
   const { body, title, date, onInputChange, formState } = useForm(note);
   const dateString = useMemo(() => {
     const newDate = new Date(date);
@@ -19,6 +25,11 @@ export const NoteView = () => {
   useEffect(() => {
     dispatch(setActiveNote(formState));
   }, [formState]);
+
+  useEffect(() => {
+    if (messageSaved.length > 0)
+      Swal.fire("Nota actualizada", messageSaved, "success");
+  }, [messageSaved]);
 
   const onSaveNote = () => {
     dispatch(startSaveNote());
@@ -40,7 +51,12 @@ export const NoteView = () => {
       </Grid>
 
       <Grid item>
-        <Button color="primary" sx={{ padding: 2 }} onClick={onSaveNote}>
+        <Button
+          disabled={isSaving}
+          color="primary"
+          sx={{ padding: 2 }}
+          onClick={onSaveNote}
+        >
           <SaveOutlined sx={{ fontSize: 30, mr: 1 }} />
           Guardar
         </Button>
--- src/store/journal/journalSlice.js
+++ src/store/journal/journalSlice.js
@@ -25,23 +25,24 @@ export const journalSlice = createSlice({
     },
     setActiveNote: (state, action) => {
       state.active = action.payload;
+      state.messageSaved = "";
     },
     setNotes: (state, action) => {
       state.notes = action.payload;
     },
     setSaving: (state) => {
       state.isSaving = true;
-      // TODO: mensaje de error...
+      state.messageSaved = "";
     },
     updateNote: (state, action) => {
-      state.isSaving = true;
+      state.isSaving = false;
       state.notes = state.notes.map((note) => {
         if (note.id === action.payload.id) {
           return action.payload;
         }
         return note;
       });
-      // TODO: Mostrar mensajes de errores
+      state.messageSaved = `${action.payload.title}, actualizada correctamente`;
     },
     deleteNoteById: (state, action) => {},
   },
