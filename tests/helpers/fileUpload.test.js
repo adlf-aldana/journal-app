ae8b0ecae42f81f564e62d14e54739ef57845e2a
Adolfo Aldana <adlf.aldana@gmail.com>
2024-04-14 00:25:06 -0400

    #add. cloudinary SDK - Delete image

--- tests/helpers/fileUpload.test.js
+++ tests/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
+import { v2 as cloudinary } from "cloudinary";
 import { fileUpload } from "../../src/helpers/fileUpload";
+
+cloudinary.config({
+  cloud_name: "drfhtcvcn",
+  api_key: "972673847792277",
+  api_secret: "u2SJbaBCfFuGmaX_mtfcCh_kUUo",
+  secure: true,
+});
 
 describe("Pruebas en fileUpload", () => {
   test("debe de subir el archivo correctamente a cloudinary", async () => {
@@ -10,5 +18,16 @@ describe("Pruebas en fileUpload", () => {
 
     const url = await fileUpload(file);
     expect(typeof url).toBe("string");
+
+    const segments = url.split("/");
+    const imageId = segments[segments.length - 1].replace(".jpg", "");
+
+    await cloudinary.api.delete_resources(["journal-app/" + imageId]);
+  });
+
+  test("debe de retornar null", async () => {
+    const file = new File([], "foto.jpg");
+    const url = await fileUpload(file);
+    expect(url).toBe(null);
   });
 });
